a6a9e69dd3ae4e44eb22e5c47db2db0e16a479a0
MayaRYS <116630259+MayaRYS@users.noreply.github.com>
2025-03-26 11:57:09 +0100

    Add files via upload
    aktueller spielstand

---
+++ data/tileset.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="tileset" tilewidth="120" tileheight="120" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="9">
+  <image source="../graphics/environment/Mitte ohne Rand unten.png" width="120" height="120"/>
+ </tile>
+ <tile id="10">
+  <image source="../graphics/environment/Mittelstück.png" width="120" height="120"/>
+ </tile>
+ <tile id="11">
+  <image source="../graphics/environment/andere ecke negativ ecke anders halt.png" width="120" height="120"/>
+ </tile>
+ <tile id="12">
+  <image source="../graphics/environment/Kante.png" width="120" height="120"/>
+ </tile>
+ <tile id="13">
+  <image source="../graphics/environment/Grass Büschel.png" width="120" height="120"/>
+ </tile>
+ <tile id="14">
+  <image source="../graphics/environment/Grass 5.png" width="120" height="120"/>
+ </tile>
+ <tile id="15">
+  <image source="../graphics/environment/Grass 3.png" width="120" height="120"/>
+ </tile>
+ <tile id="16">
+  <image source="../graphics/environment/Erde ganz.png" width="120" height="120"/>
+ </tile>
+ <tile id="17">
+  <image source="../graphics/environment/Ecke.png" width="120" height="120"/>
+ </tile>
+ <tile id="18">
+  <image source="../graphics/environment/Ecke ohne rand unten.png" width="120" height="120"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../../Downloads/gras.png" width="120" height="120"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../../Downloads/gras_daisies.png" width="120" height="120"/>
+ </tile>
+ <tile id="21">
+  <image source="../../../../Downloads/gras_2.png" width="120" height="120"/>
+ </tile>
+</tileset>
